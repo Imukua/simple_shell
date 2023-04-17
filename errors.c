33319beb0f8a9aeac18bd444db0f41780b2a28b8
Imukua <ianmukuaa@gmail.com>
2023-04-17 19:20:24 +0300

    first running point"

---
+++ errors.c
@@ -0,0 +1,34 @@
+#include "headers.h"
+/*void handle_input_error - handles input errors
+ * @line_value: value returned by getline
+ * Return: void
+*/
+void handle_input_error(int line_value)
+{
+	if (line_value == -1)
+	{
+	if (feof(stdin))
+	{
+	exit(EXIT_SUCCESS);
+	} else
+	{
+	perror("readline");
+	exit(EXIT_FAILURE);
+	}
+	}
+}
+
+/*void check_malloc_er - checks if malloc failed
+ * @ptr: pointer to the memory allocated
+ * Return: void
+ */
+void check_malloc_er(void *ptr)
+{
+	if (ptr == NULL)
+	{
+	perror("malloc");
+	exit(EXIT_FAILURE);
+	}
+}
+
+
---
+++ execution.c
@@ -0,0 +1,61 @@
+#include "headers.h"
+
+/**
+* print_prompt - prints the prompt
+* @prompt: the prompt to be printed
+*
+* Return: void
+*/
+void print_prompt(char *prompt)
+{
+	for (int i = 0; prompt[i] != '\0'; i++)
+	{
+	_putchar(prompt[i]);
+	}
+}
+
+/**
+ * execmd - executes a command
+ * @argv: array of pointers to the arguments
+ *
+ * Return: return value of the last executed command
+*/
+void execmd(char **argv)
+{
+	char *execution = NULL;
+
+	if (argv)
+	{
+	execution = argv[0];
+
+	/* execute the command with execve */
+	if (execve(execution, argv, NULL) == -1)
+	{
+	perror("Error:");
+	};
+	}
+
+}
+
+/**
+ * tokenize_input - tokenizes the input string
+ * @input_str: input string
+ * @tokens: array of pointers to the tokens
+ * @delim: delimiter
+ * @max_tokens: maximum number of tokens
+ *
+ * Return: number of tokens
+*/
+int tokenize_input(char *input_str, char **tokens,
+const char *delim, int max_tokens)
+{
+	char *token = strtok(input_str, delim);
+	int token_count = 0;
+
+	while (token != NULL && token_count < max_tokens)
+	{
+		tokens[token_count++] = token;
+		token = strtok(NULL, delim);
+	}
+	return (token_count);
+}
